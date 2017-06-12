27247e15b4fe8cf233958dad6cae03d945162240
Omar Shekfeh <o.shekfeh@gmail.com>
2017-06-12 23:48:40 +0300

    Start, part3 after creating template folder

---
+++ polls/views.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from .models import Question
+
+# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You are looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voitng on question %s." % question_id)
